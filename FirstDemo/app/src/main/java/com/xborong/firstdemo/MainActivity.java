f360368c597fb749905e0a9ad9a863aaf41f6a6c
xborong <xborong@qq.com>
2019-03-13 16:36:45 +0800

    【New】匿名类对象获取外部变量（闭包）

--- FirstDemo/app/src/main/java/com/xborong/firstdemo/MainActivity.java
+++ FirstDemo/app/src/main/java/com/xborong/firstdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.xborong.firstdemo;
 
+import android.Manifest;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,17 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
         Button button1 = findViewById(R.id.button_1);
+        final String title = "点它";
+        button1.setText(title);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Button btn = (Button)v;
-                if (btn.getText().equals("点我")) {
+                if (btn.getText().equals(title)) {
                     btn.setText("再点啊！");
                 } else {
-                    btn.setText("点我");
+                    btn.setText(title);
                 }
-                MainActivity.this.setTitle("成功了！");
-                Toast.makeText(MainActivity.this, "成功了！", Toast.LENGTH_LONG).show();
+                MainActivity.this.setTitle("你被点了！");
+                Toast.makeText(MainActivity.this, "成功了！", Toast.LENGTH_SHORT).show();
             }
         });
     }
